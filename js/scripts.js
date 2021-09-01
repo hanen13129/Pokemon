248ee1002e6a31046decfe1cb2eb9609c7715271
hanen13129 <benhassenhanen@gmail.com>
2021-09-01 20:05:55 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood='pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  {name:'Bulbasaur', height:7,types:['grass', 'poison']},
+  {name:'Metagross', height:5,types:['Steel','Psychic']},
+  {name:'Scizor', height:5,types:['Steel', 'Bug']}
+];
