3bdaddb36ac4c9fc43dcf295a995ba2604d9ea52
hanen13129 <benhassenhanen@gmail.com>
2021-09-06 22:24:22 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 let pokemonList = [
-  {name:'Bulbasaur', height:7,types:['grass', 'poison']},
-  {name:'Metagross', height:5,types:['Steel','Psychic']},
-  {name:'Scizor', height:5,types:['Steel', 'Bug']}
+  { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
+  { name: 'Metagross', height: 5, types: ['Steel', 'Psychic'] },
+  { name: 'Scizor', height: 5, types: ['Steel', 'Bug'] }
 ];
-for(let i =0; i<=pokemonList.length; i++){
+for (let i = 0; i < pokemonList.length; i++) {
   let pokemonName = pokemonList[i].name;
   let pokemonHeight = pokemonList[i].height;
   let pokemonTypes = pokemonList[i].types;
-  if (pokemonHeight>=7){
-   document.write( '« '+ pokemonName+' (Height :'+pokemonHeight + ' )' + "-Wow that's big !" +' ». </p>' );
+  if (pokemonHeight >= 7) {
+    document.write('« ' + pokemonName + ' (Height :' + pokemonHeight + ' )' + "-Wow that's big !" + ' ». </p>');
   }
-  else
-   {
-   document.write( '« '+ pokemonName+' (Height :'+pokemonHeight + ' ) ».</p>');
-   }
+  else {
+    document.write('« ' + pokemonName + ' (Height :' + pokemonHeight + ' ) ».</p>');
+  }
 
 }
+
+
+
+
+
