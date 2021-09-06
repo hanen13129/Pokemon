89c22f91f07d99ab7c799f6b21ba5e56df580e29
hanen13129 <benhassenhanen@gmail.com>
2021-09-06 09:17:45 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,16 @@ let pokemonList = [
   {name:'Metagross', height:5,types:['Steel','Psychic']},
   {name:'Scizor', height:5,types:['Steel', 'Bug']}
 ];
+for(let i =0; i<=pokemonList.length; i++){
+  let pokemonName = pokemonList[i].name;
+  let pokemonHeight = pokemonList[i].height;
+  let pokemonTypes = pokemonList[i].types;
+  if (pokemonHeight>=7){
+   document.write( '« '+ pokemonName+' (Height :'+pokemonHeight + ' )' + "-Wow that's big !" +' ». </p>' );
+  }
+  else
+   {
+   document.write( '« '+ pokemonName+' (Height :'+pokemonHeight + ' ) ».</p>');
+   }
+
+}
