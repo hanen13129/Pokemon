5bca278e7687830aa9955f3a2b68556ef17d3eae
hanen13129 <benhassenhanen@gmail.com>
2021-09-14 08:33:34 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,65 @@
+let pokemonRepository = (function () {
+
 let pokemonList = [
   { name: 'Bulbasaur', height: 7, types: ['grass', 'poison'] },
   { name: 'Metagross', height: 5, types: ['Steel', 'Psychic'] },
   { name: 'Scizor', height: 5, types: ['Steel', 'Bug'] }
 ];
-for (let i = 0; i < pokemonList.length; i++) {
-  let pokemonName = pokemonList[i].name;
-  let pokemonHeight = pokemonList[i].height;
-  let pokemonTypes = pokemonList[i].types;
+function getAll() {
+    return pokemonList;
+  }
+
+  function add(item) {
+    return pokemonList.push(item);
+  }
+
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  let pokemonName = pokemon.name;
+  let pokemonHeight = pokemon.height;
+
   if (pokemonHeight >= 7) {
-    document.write('« ' + pokemonName + ' (Height :' + pokemonHeight + ' )' + "-Wow that's big !" + ' ». </p>');
+    document.write(
+      '<p>' +
+        pokemonName +
+        ' ' +
+        '(Height: ' +
+        pokemonHeight +
+        ')' +
+        " - Wow that's big!" +
+        '</p>'
+    );
+    console.log(
+      pokemonName +
+        ' ' +
+        '(Height: ' +
+        pokemonHeight +
+        ')' +
+        " - Wow that's big!"
+    );
+  } else {
+    document.write(
+      '<p>' + pokemonName + ' ' + '(Height: ' + pokemonHeight + ')' + '</p>'
+    );
+    console.log(pokemonName + ' ' + '(Height: ' + pokemonHeight + ')');
   }
-  else {
-    document.write('« ' + pokemonName + ' (Height :' + pokemonHeight + ' ) ».</p>');
-  }
+});
+// for (let i = 0; i < pokemonList.length; i++) {
+//   let pokemonName = pokemonList[i].name;
+//   let pokemonHeight = pokemonList[i].height;
+//   let pokemonTypes = pokemonList[i].types;
+//   if (pokemonHeight >= 7) {
+//     document.write('« ' + pokemonName + ' (Height :' + pokemonHeight + ' )' + "-Wow that's big !" + ' ». </p>');
+//   }
+//   else {
+//     document.write('« ' + pokemonName + ' (Height :' + pokemonHeight + ' ) ».</p>');
+//   }
 
-}
-
-
-
+// }
 
 
